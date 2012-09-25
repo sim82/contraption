3971358ddd631fb7570378581eb679196bdb442f
Simon Berger <simberger@gmail.com>
2012-09-25 17:36:55 +0200

    small updates.

---
+++ build_versions.h
@@ -0,0 +1,13 @@
+
+
+namespace versions {
+    namespace visual_papara {
+        const int major = 1;
+        const int minor = 0;
+    }
+    
+    namespace epa_viewer {
+        const int major = 1;
+        const int minor = 0;
+    }
+}
